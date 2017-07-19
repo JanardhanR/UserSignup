e7b37cf8c1600327ee850431b7b8784464264633
JanardhanR <jana.dhana@outlook.com>
2017-07-20 05:28:07 +0530

    in progress for cookie

--- main.py
+++ main.py
@@ -4,10 +4,39 @@ import webapp2
 import string
 import codecs
 import re
+import hashlib
+import string
+from google.appengine.ext import db
+
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+
+def hash_str(s):
+    return hashlib.md5(s).hexdigest()
+
+def make_secure_val(s):
+    return "%s,%s" % (s,hash_str(s))
+
+def check_secure_val(h):
+    val = h.split(',')[0]
+    if h == make_secure_val(val):
+        return val
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
 
 class AuthCred():
@@ -28,17 +57,6 @@ class AuthCred():
         return self.EMAIL.match(email)
 
 
-class Handler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
-
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
-
 class Welcome(Handler):
     def get(self):
         username = self.request.get("username")
@@ -49,10 +67,11 @@ class SignUp(Handler):
         self.render("usersignup.html")
     
     def post(self):
-        username = self.request.get("username")
-        password = self.request.get("password")
-        verify = self.request.get("verify")
-        email = self.request.get("email")
+        # username = self.request.get("username")
+        # password = self.request.get("password")
+        # verify = self.request.get("verify")
+        # email = self.request.get("email")
+        self.resquest.cookie.get('username')
         usererror =""
         verifyerror =""
         emailerror =""
